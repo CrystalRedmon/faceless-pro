af5e291018b8c7a567ce633028bda043902f1316
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2022-12-12 15:01:44 -0600

    initial set up for client side. post and fetch jobs

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -32,6 +32,10 @@ function Nav() {
               Info Page
             </Link>
 
+            <Link className="navLink" to="/job">
+              Post Job
+            </Link>
+
             <LogOutButton className="navLink" />
           </>
         )}
---
+++ src/components/PostJob/PostJob.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { useDispatch } from 'react-redux';
+import { useHistory } from "react-router-dom";
+function PostJob(){
+    const dispatch = useDispatch();
+    const history = useHistory()
+
+    const [employeeId, setEmployeeId] = useState('');
+    const [title, setTitle] = useState('');
+    const [description, setDescription ] = useState('')
+    const submitPost = () =>{
+        console.log('Posting job')
+        dispatch({
+            type: "POST_JOB",
+            payload: form
+        })
+        history.push('/')
+    }
+    const form = {
+        employeeId,
+        title,
+        description
+    }
+    const handleId = (evt) =>{
+        setEmployeeId(evt.target.value);
+    }
+    const handleTitle = (evt) =>{
+        setTitle(evt.target.value);
+    }
+    const handleDescription = (evt) =>{
+        setDescription(evt.target.value);
+    }
+   
+
+    return(
+        <>
+        <h2>Post New Job</h2>
+        <label htmlFor="id">Emp Id</label>
+        <input type="text" value={employeeId} onChange={handleId}/>
+        <label htmlFor="">Title</label>
+        <input type="text" value={title} onChange={handleTitle} />
+        <label htmlFor="">Description</label>
+        <textarea type="text" value={description} onChange={handleDescription} />
+        <button onClick={submitPost}>Submit</button>
+
+        </>
+    )
+}
+
+export default PostJob;
