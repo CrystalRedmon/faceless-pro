4b700e13abf428521f61aba255ef6ec9de7e49fb
Crystal Redmon <cato2848@gmail.com>
2022-12-13 13:05:36 -0600

    complete get/:id and delete

--- server/routes/employer.router.js
+++ server/routes/employer.router.js
@@ -3,52 +3,99 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 
+// GET ALL JOB POSTS BY EMPLOYER ID/[req.user.user_info.id] 
 router.get('/', (req, res) => {
 
   const sqlTxt = `SELECT "job_post".title, "job_post".description
                   FROM "job_post"
-                  WHERE "job_post"."employer_id" = $1;`;              
-                  
-               
-    pool.query(sqlTxt, [req.user.user_info.id])    //💬 [req.user.user_info.id] === employer_id 
-    .then(dbRes=>{
-        res.send(dbRes.rows);
-        console.log(dbRes.rows);
+                  WHERE "job_post"."employer_id" = $1;`;
+
+
+  pool.query(sqlTxt, [req.user.user_info.id])    //💬 [req.user.user_info.id] === employer_id 
+    .then(dbRes => {
+      res.send(dbRes.rows);
+      console.log(dbRes.rows);
     })
-    .catch(error=>{
-        res.sendStatus(500);
-        console.log('GET positions failed: ', error);
+    .catch(error => {
+      res.sendStatus(500);
+      console.log('GET positions failed: ', error);
+    })
+});
+
+// GET SPECIFIC JOB POSTS BY JOB_POST.ID  AND EMPLOYER ID/[req.user.user_info.id] 
+router.get('/:id', (req, res) => {
+
+  const sqlTxt = `SELECT "job_post".title, "job_post".description
+                  FROM "job_post"
+                  WHERE "job_post"."employer_id" = $1
+                  AND "job_post"."id" = $2;`;
+
+  const sqlParams = [
+    req.user.user_info.id,    //💬 [req.user.user_info.id] === employer_id 
+    req.params.id
+  ]
+
+  pool.query(sqlTxt, sqlParams)
+    .then(dbRes => {
+      res.send(dbRes.rows);
+      console.log(dbRes.rows);
+    })
+    .catch(error => {
+      res.sendStatus(500);
+      console.log('GET positions failed: ', error);
     })
 });
 
 
-
+// CREATE JOB POSTS
 router.post('/', (req, res) => {
- 
-const sqlTxt = `INSERT INTO "job_post" ("employer_id", "title", "description")
+
+  const sqlTxt = `INSERT INTO "job_post" ("employer_id", "title", "description")
                 VALUES ($1, $2, $3);`
 
-const sqlParams = [
-  req.user.user_info.id,
-  req.body.title,
-  req.body.description
-]
+  const sqlParams = [
+    req.user.user_info.id,
+    req.body.title,
+    req.body.description
+  ]
 
-     pool.query(sqlTxt, sqlParams )
-     .then(dbRes=>{
+  pool.query(sqlTxt, sqlParams)
+    .then(dbRes => {
       res.sendStatus(200);
       console.log('Post job successful: ', dbRes);
-     })      
-     .catch(error=>{
+    })
+    .catch(error => {
       res.sendStatus(500);
       console.log('Post job failed: ', error);
-     })
+    })
 
 });
 
 
+// DELETE JOB POST BY ID
+router.delete('/:id', (req, res) => {
+  const sqlTxt = `DELETE 
+                  FROM "job_post"
+                  WHERE "job_post"."employer_id" = $1
+                  AND "job_post"."id" = $2;`;
+
+  const sqlParams = [
+    req.user.user_info.id,
+    req.params.id
+  ]
 
 
+  pool.query(sqlTxt, sqlParams)
+    .then(dbRes => {
+      res.sendStatus(200);
+      console.log('Delete job successful: ', dbRes);
+    })
+    .catch(error => {
+      res.sendStatus(500);
+      console.log('Delete job failed: ', error);
+    })
+
+})
 
 
 
