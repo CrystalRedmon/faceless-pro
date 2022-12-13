015f814fc5d12a6690124571e923752f4b201d75
Daniel Pathammavong <patha037@umn.edu>
2022-12-13 14:13:17 -0600

    employer registration GET and Post Routes

---
+++ server/routes/employerProfile.router.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route template
+ */
+ router.get('/', (req, res) => {
+  // GET route code here
+
+  const sqlText = `SELECT *FROM "employer"
+  WHERE "user_id" = $1;`;
+
+  pool.query(sqlText)
+  .then((result) =>{
+   // console.log('result is:',result.rows)
+   res.send(result.rows[0]) 
+   console.log(result.rows);
+  })
+  .catch((error) =>{
+   console.log('error fetching items', error)
+   res.sendStatus(500)
+  })
+
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+  // POST route code here
+  const sqlText = `INSERT INTO "employer" (user_id, company_name, company_address, company_phone, logo_path, company_description, company_link) VALUES ($1, $2, $3, $4, $5, $6, $7)`;
+
+  pool
+  .query(sqlText, [req.body.user_id, req.body.company_name, req.body.company_address, req.body.company_phone, req.body.logo_path, req.body.company_description, req.body.company_link])
+  .then((queryResponse) => res.send(queryResponse))
+  .catch((err) => {
+    console.log('Employer Post info failed ', err);
+    res.sendStatus(500);
+  });
+});
+
+module.exports = router;
