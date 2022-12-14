603b0a558bbabf7e4fd8d4548427d1fac431abd8
Daniel Legan <legan.daniel@gmail.com>
2022-12-14 10:39:07 -0600

    syntax fix from merge

--- src/components/EmployerLandingPage/EmployerLandingPage.jsx
+++ src/components/EmployerLandingPage/EmployerLandingPage.jsx
@@ -3,8 +3,11 @@ import EmployerJobList from "../EmployerJobList/EmployerJobList";
 
 function EmployerLandingPage() {
     return (
-        <h1>Employer Profile</h1>
-        <EmployerJobList/>
+        <>
+            <h1>Employer Profile</h1>
+            <EmployerJobList />
+        </>
+
     );
 }
 
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -56,8 +56,6 @@ function UserPage() {
 
 
        <EmployerJobList/>
-
-       }
     
       <LogOutButton className="btn" />
     </div>
