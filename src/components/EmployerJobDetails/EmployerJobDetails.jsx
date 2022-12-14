a4538092d9584ff54193f77af1f9fc0a31c46b69
Crystal Redmon <cato2848@gmail.com>
2022-12-14 05:43:38 -0600

    dispatch params.id to fetch current job post

--- src/components/EmployerJobDetails/EmployerJobDetails.jsx
+++ src/components/EmployerJobDetails/EmployerJobDetails.jsx
@@ -9,20 +9,21 @@ function EmployerJobDetails(){
     const params = useParams();
     const dispatch = useDispatch();
     const job = useSelector(store=>store.currentJob);
-
+    console.log('current job details: ', job);
 
     //FETCH CURRENT JOB WITH PARAMS ID
     useEffect(()=>{
         dispatch({
             type: 'FETCH_CURRENT_JOB_POST',
+            payload: `${params.id}`
         })
-
+        console.log('params', params.id);
 
     })
 
     return<>
     
-    <h1>Here we Go</h1>
+    <h1>Details</h1>
     
     
 </>}
