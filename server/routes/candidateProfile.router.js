30a5c3546a3eecf466c016044d4e8499f6616d6f
Daniel Pathammavong <patha037@umn.edu>
2022-12-13 22:09:29 -0600

    Added two get routes for candidate

--- server/routes/candidateProfile.router.js
+++ server/routes/candidateProfile.router.js
@@ -26,12 +26,28 @@ router.get('/', (req, res) => {
       })
   });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
-});
+
+ 
+
+ // GET to the jobs details page in the Candidate Landing Page.
+ router.get('/:id', (req, res) => {
+
+    const sqlTxt = `SELECT "employer".company_name,"employer".logo_path,"job_post".title,"job_post".description
+    FROM "job_post"
+    JOIN "employer"
+        on "employer".id = "job_post".employer_id
+    WHERE "job_post".id = $1;`;
+  
+    pool.query(sqlTxt, [req.params.id])
+      .then(dbRes => {
+        res.send(dbRes.rows);
+        console.log(dbRes.rows);
+      })
+      .catch(error => {
+        res.sendStatus(500);
+        console.log('GET positions failed: ', error);
+      })
+  });
 
 module.exports = router;
 
