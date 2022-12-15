751a6af17eff7f1302fdf6f999451df6292b5267
Crystal Redmon <cato2848@gmail.com>
2022-12-15 15:34:51 -0600

    create put request to update candidate basic info

--- server/routes/candidateInfo.router.js
+++ server/routes/candidateInfo.router.js
@@ -30,6 +30,48 @@ router.post('/', (req, res)=>{
 
 
 
+router.put('/:id', (req, res)=>{
+
+    console.log('Candidate14: ', req.user, req.body)
+    
+    const sqlTxt = `UPDATE "candidate"
+                    SET "first_name" = $1,
+                    "last_name" = $2,
+                    "linkedin_link" = $3,
+                    "resume_path" = $4,
+                    "cover_letter_path" = $5
+                    WHERE "id" = $6;`;
+
+    const sqlParams = [
+        req.body.first_name,
+        req.body.last_name,
+        req.body.linkedin_link,
+        req.body.resume_path,
+        req.body.cover_letter_path,
+        req.user.id
+    ]
+
+
+    pool.query(sqlTxt, sqlParams)
+    .then(result=>{
+        res.sendStatus(200);
+        console.log('PUT candidate info successful');
+    })
+    .catch(error=>{
+        res.sendStatus(500);
+        console.log('POST candidate info failed: ', error);
+    })
+
+})
+
+
+
+
+
+
+
+
+
 
 
 
