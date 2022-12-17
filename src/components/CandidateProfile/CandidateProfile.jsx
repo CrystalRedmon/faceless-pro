26bf533716eab3ff0e57ee01c86e7e68edee1e4d
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2022-12-17 15:57:30 -0600

    removed form from candidate profile

--- src/components/CandidateProfile/CandidateProfile.jsx
+++ src/components/CandidateProfile/CandidateProfile.jsx
@@ -20,47 +20,10 @@ function CandidateProfile() {
         })
         history.push('/StartProfile');
     }
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [linkedIn, setLinkedIn] = useState('');
-    const [resume, setResume] = useState('');
-    const [coverLetter, setCoverLetter] = useState('');
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log('in handleSubmit');
-
-        dispatch({
-            type: 'ADD_EMPLOYER_INFO',
-            payload: {
-                company_name: companyName,
-                company_address: companyAddress,
-                company_phone: companyPhone,
-                logo_path: companyLogoPath,
-                company_description: companyDescription,
-                company_link: companyLink,
-            }
-        })
-
-        setCompanyName('');
-        setCompanyAddress('');
-        setCompanyPhone('');
-        setCompanyLink('');
-        setCompanyLogoPath('');
-        setCompanyDescription('');
-    }
 
     return (
         <>
             <h2>Welcome to your profile!</h2>
-            <form onSubmit={handleSubmit}>
-            <TextField value={firstName} onChange={(event) => {setFirstName(event.target.value)} } label="First Name" />
-            <TextField value={lastName} onChange={(event) => { setLastName(event.target.value) }} label="Last Name" />
-            <TextField value={linkedIn} onChange={(event) => { setLinkedIn(event.target.value) }} label="LinkedIn" />
-            <TextField value={resume} onChange={(event) => { setResume(event.target.value) }} label="Resume" />
-            <TextField value={coverLetter} onChange={(event) => { setCoverLetter(event.target.value) }} label="company logo upload" />
-            <Button type='submit' variant='contained'>submit</Button>
-        </form>
 
             <p>Click here to complete your profile</p>
 
