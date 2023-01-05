9d159dc20a7bae871d298253f86e42487e97793f
Crystal Redmon <cato2848@gmail.com>
2023-01-04 18:30:43 -0600

    add grid and spacing to candidateLandingPage

--- src/components/CandidateLandingPage/CandidateLandingPage.jsx
+++ src/components/CandidateLandingPage/CandidateLandingPage.jsx
@@ -3,62 +3,74 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './CandidateLandingPage.css';
 import CandidateJobItem from '../CandidateJobItem/CandidateJobItem';
+import { Box, Grid } from '@mui/material';
 
 function CandidateLandingPage() {
-    const recentJobs = useSelector(store => store.candidateReducer.candidateJobs)
-    const [keyword, setKeyword] = useState('');
-    const [searchJobsClicked, setSearchJobsClicked] = useState(false);
+  const recentJobs = useSelector(store => store.candidateReducer.candidateJobs)
+  const [keyword, setKeyword] = useState('');
+  const [searchJobsClicked, setSearchJobsClicked] = useState(false);
 
-    const history = useHistory();
-    const dispatch = useDispatch();
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_RECENT_JOBS' });
-    }, []);
+  useEffect(() => {
+    dispatch({ type: 'FETCH_RECENT_JOBS' });
+  }, []);
 
-    const handleKeyword=(evt) => {
-        setKeyword(evt.target.value)
-        console.log(evt)
-       }
-    function onSubmitKeyword(){
-        console.log("keyword",keyword);   
-          setSearchJobsClicked(true) // This is called a not operator '!'
-        dispatch({
-          type: 'FETCH_SEARCHED_JOBS',
-          payload: keyword
-      });
+  const handleKeyword = (evt) => {
+    setKeyword(evt.target.value)
+    console.log(evt)
+  }
+  function onSubmitKeyword() {
+    console.log("keyword", keyword);
+    setSearchJobsClicked(true) // This is called a not operator '!'
+    dispatch({
+      type: 'FETCH_SEARCHED_JOBS',
+      payload: keyword
+    });
 
-      }
+  }
 
-    console.log("This is the recent jobs", recentJobs)
-    return (
-        <div>
+  console.log("This is the recent jobs", recentJobs)
+  return (
+    <div>
+      <Box sx={{ textAlign: 'center' }}>
+
         <h1>Candidate Search for Jobs HERE</h1>
         <form>
-        <input placeholder = "Search keywords" value = {keyword} onChange={handleKeyword}></input>
+          <input placeholder="Search keywords" value={keyword} onChange={handleKeyword}></input>
 
-        <button onClick={onSubmitKeyword}>Search</button>
-        {searchJobsClicked ? <button onClick={() => {
-          setSearchJobsClicked(false)
-          dispatch({
-            type: 'FETCH_RECENT_JOBS',
-          })
-        }}> Undo Search </button> : <> </>
+          <button onClick={onSubmitKeyword}>Search</button>
+          {searchJobsClicked ? <button onClick={() => {
+            setSearchJobsClicked(false)
+            dispatch({
+              type: 'FETCH_RECENT_JOBS',
+            })
+          }}> Undo Search </button> : <> </>
 
-        }
+          }
 
-      </form>
+        </form>
+      </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={5.25}></Grid>
 
-      <section>
-        {searchJobsClicked ? <h3>Searched Jobs</h3>
-        :
-             <h3>Recently Added Jobs</h3>}
-             
-              {recentJobs.map(job =>
-                    <CandidateJobItem key={job.id} job={job} />
-                )}
-          </section>
 
+        <Grid item xs={6.75}>
+          {searchJobsClicked ? <h3>Searched Jobs</h3>
+            :
+            <h3>Recently Added Jobs</h3>}
+        </Grid>
+        <Grid item xs={2}></Grid>
+        <Grid container item xs={10}>
+          {recentJobs.map(job =>
+            <CandidateJobItem key={job.id} job={job} />
+          )}
+        </Grid>
+
+
+
+      </Grid>
     </div>
   );
 }
