d2a472d7dda3d4a4dc744ae3ec855c04d4c5a3ed
Crystal Redmon <cato2848@gmail.com>
2023-01-05 16:55:40 -0600

    add mui grid and cards to candidateAppliedJobsPage

--- src/components/CandidateAppliedJobsPage/CandidateAppliedJobsPage.jsx
+++ src/components/CandidateAppliedJobsPage/CandidateAppliedJobsPage.jsx
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 function AppliedToJobsPage() {
   const history = useHistory();
   const params = useParams();
@@ -22,152 +23,156 @@ function AppliedToJobsPage() {
   }, []);
 
 
-const useStyles = makeStyles({
-  table: {
-    tableLayout: "fixed",
-    width: "100%",
-  },
-  td: {
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap",
-  },
-});
+  const useStyles = makeStyles({
+    table: {
+      tableLayout: "fixed",
+      width: "100%",
+    },
+    td: {
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+    },
+  });
 
   return (
     <>
-      <h1>AppliedToJobsPage - CANDIDATE</h1>
-      <section>
-        {appliedJobsList.map((job) => {
-          return (
-            <Box
-              key={job.id}
-              sx={{
-                border: "solid black 2px",
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
-              <Box>
-                {" "}
-                <h4
-                  onClick={() => {
-                    history.push(`/job/${job.id}`);
-                    dispatch({
-                      type: "VIEW_JOB_DETAILS",
-                      payload: job,
-                    });
-                  }}
-                >
-                  {" "}
-                  {job.title}{" "}
-                </h4>
-              </Box>
-              <Box>
-                <p> {job.company_name}</p>
-              </Box>
-              <Box>
-                <p> {job.logo_path} </p>
-              </Box>
-              <Box>
-                <p> {job.company_address}</p>
-              </Box>
-              <Box>
-                {" "}
-                {job.status === "pending" ? (
-                  <p>Applied on {new Date(job.time).toLocaleString()}</p>
-                ) : (
-                  <></>
-                )}
-              </Box>
-              <Box>
-                {job.status === "not_shared" ? (
-                  <>
-                    <p>Applied on {new Date(job.time).toLocaleString()}</p>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={() => {
-                        dispatch({
-                          type: "SET_JOB_MESSAGE",
-                          payload: job.id,
-                        });
-                        history.push(`/message/${job.id}`);
-                        console.log("application id:", job.id);
-                      }}
-                    >
-                      Chat
-                    </Button>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={() => {
-                        console.log("Sharing info for job id:",job.id)
-                        dispatch({
-                          type: "SHARE_INFO",
-                          payload: job.id
-                          
-                        })
-                      }}
-                    >
-                      Share Information
-                    </Button>
-                  </>
-                ) : (
-                  <></>
-                )}
-              </Box>
-              <Box>
-                {job.status === "shared" ? (
-                  <>
-                    <p>Person info shared with employer-Applied on {new Date(job.time).toLocaleString()}</p>
-                    <br />
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={() => {
-                        dispatch({
-                          type: "SET_JOB_MESSAGE",
-                          payload: job.id,
-                        });
-                        history.push(`/message/${job.id}`);
-                        console.log("application id:", job.id);
-                      }}
-                    >
-                      Chat
-                    </Button>
-                  </>
-                ) : (
-                  <></>
-                )}
-              </Box>
-              <Box>
-                {job.status === "rejected" ? (
-                  <>
-                    <p>Applied on {new Date(job.time).toLocaleString()}</p>
-                    <br />
-                    <p>Not selected for this position</p>
-                  </>
-                ) : (
-                  <></>
-                )}
-              </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={8}><h1>AppliedToJobsPage - CANDIDATE</h1></Grid>
 
-              <button
-                onClick={() => {
-                  history.push(`/CandidateJobDetails/${job.id}`);
-                  dispatch({
-                    type: "VIEW_JOB_DETAILS",
-                    payload: `${params.id}`,
-                  });
-                }}
-              >
-                DETAILS
-              </button>
-            </Box>
-          );
-        })}
-      </section>
+        <Grid item xs={3}></Grid>
+        <Grid sx={{marginTop: 4}}>
+          <section>
+            {appliedJobsList.map((job) => {
+              return (
+                <Grid container >
+                  <Box
+                    key={job.id}
+                    sx={{ width: '50em', borderRadius: '5px', boxShadow: 3, padding: '10px', border: 'solid grey 1px', display: "flex", justifyContent: 'space-between', marginBottom: 2 }}
+                  >
+                    <Box>
+                      {" "}
+                      <h4
+                        onClick={() => {
+                          history.push(`/job/${job.id}`);
+                          dispatch({
+                            type: "VIEW_JOB_DETAILS",
+                            payload: job,
+                          });
+                        }}
+                      >
+                        {" "}
+                        {job.title}{" "}
+                      </h4>
+                    </Box>
+                    <Box>
+                      <p>{job.company_name}</p>
+                    </Box>
+                    <Box>
+                      <p> {job.logo_path} </p>
+                    </Box>
+                    <Box>
+                      <p> {job.company_address}</p>
+                    </Box>
+                    <Box>
+                      {" "}
+                      {job.status === "pending" ? (
+                        <p>Applied on {new Date(job.time).toLocaleString()}</p>
+                      ) : (
+                        <></>
+                      )}
+                    </Box>
+                    <Box>
+                      {job.status === "not_shared" ? (
+                        <>
+                          <p>Applied on {new Date(job.time).toLocaleString()}</p>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={() => {
+                              dispatch({
+                                type: "SET_JOB_MESSAGE",
+                                payload: job.id,
+                              });
+                              history.push(`/message/${job.id}`);
+                              console.log("application id:", job.id);
+                            }}
+                          >
+                            Chat
+                          </Button>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={() => {
+                              console.log("Sharing info for job id:", job.id)
+                              dispatch({
+                                type: "SHARE_INFO",
+                                payload: job.id
+
+                              })
+                            }}
+                          >
+                            Share Information
+                          </Button>
+                        </>
+                      ) : (
+                        <></>
+                      )}
+                    </Box>
+                    <Box>
+                      {job.status === "shared" ? (
+                        <>
+                          <p>Person info shared with employer-Applied on {new Date(job.time).toLocaleString()}</p>
+                          <br />
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={() => {
+                              dispatch({
+                                type: "SET_JOB_MESSAGE",
+                                payload: job.id,
+                              });
+                              history.push(`/message/${job.id}`);
+                              console.log("application id:", job.id);
+                            }}
+                          >
+                            Chat
+                          </Button>
+                        </>
+                      ) : (
+                        <></>
+                      )}
+                    </Box>
+                    <Box>
+                      {job.status === "rejected" ? (
+                        <>
+                          <p>Applied on {new Date(job.time).toLocaleString()}</p>
+                          <br />
+                          <p>Not selected for this position</p>
+                        </>
+                      ) : (
+                        <></>
+                      )}
+                    </Box>
+
+                    <button
+                      onClick={() => {
+                        history.push(`/CandidateJobDetails/${job.id}`);
+                        dispatch({
+                          type: "VIEW_JOB_DETAILS",
+                          payload: `${params.id}`,
+                        });
+                      }}
+                    >
+                      DETAILS
+                    </button>
+                  </Box>
+                </Grid>);
+            })}
+          </section>
+        </Grid>
+      </Grid>
     </>
   );
 }
