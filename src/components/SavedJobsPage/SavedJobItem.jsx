2fa23f3fbdf0a453db0a9bc7ed8f3922c6f3e72b
Crystal Redmon <cato2848@gmail.com>
2023-01-08 16:44:26 -0600

    add Box to savedJobItem

--- src/components/SavedJobsPage/SavedJobItem.jsx
+++ src/components/SavedJobsPage/SavedJobItem.jsx
@@ -4,7 +4,9 @@ import { useHistory } from "react-router-dom";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { PanoramaSharp } from "@mui/icons-material";
-
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
 
 
 function SavedJobItem({ savedJob }) {
@@ -16,11 +18,83 @@ function SavedJobItem({ savedJob }) {
     );
     const history = useHistory();
 
-
+    function submitApplied() {
+        dispatch({
+            type: 'FETCH_APPLIED_JOBS',
+        });
+    }
 
     return (<>
         <h1>This is an job item</h1>
+        <Box sx={{ borderRadius: '5px', boxShadow: 3, padding: '10px', border: 'solid grey 1px', display: "flex", justifyContent: 'space-between', marginBottom: 2 }}>
 
+
+            <Box sx={{ marginBottom: 1 }}>
+                {savedJob.title}
+            </Box>
+
+            <Box sx={{ marginBottom: 1 }}>
+                {savedJob.company_name}
+            </Box>
+
+            <Box onClick={submitApplied}>
+                {appliedJobsList.find((c) => c.id === savedJob.id) ? (
+                    <p>Applied</p>
+                ) : (
+                    <Button
+                        onClick={() => {
+                            dispatch({
+                                type: "APPLY_JOB",
+                                payload: savedJob,
+                            });
+                            history.push("/applied");
+                        }}
+                    >
+                        APPLY{" "}
+                    </Button>
+                )}
+
+            </Box>
+
+
+            <Box>
+                <Button
+                    onClick={() => {
+                        history.push(`/CandidateJobDetails/${savedJob.id}`);
+                        dispatch({
+                            type: "VIEW_JOB_DETAILS",
+                            payload: `${params.id}`,
+                        });
+                    }}
+                >
+                    DETAILS
+                </Button>
+            </Box>
+
+            <Box>
+            <Button
+                onClick={() => {
+                    dispatch({
+                        type: "DELETE_JOB",
+                        payload: savedJob,
+                    });
+                }}
+            >
+                REMOVE{" "}
+            </Button>
+
+
+            </Box>
+
+
+
+
+
+
+
+
+
+        </Box>
         {/* <div key={job.id}>
             <h4> {job.title} </h4>
 
