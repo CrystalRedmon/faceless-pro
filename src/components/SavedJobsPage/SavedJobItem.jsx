f181afc96947e331a626e64260e51b913bc03de3
Crystal Redmon <cato2848@gmail.com>
2023-01-08 19:10:51 -0600

    correct sizing and spacing for savedJobItems.

--- src/components/SavedJobsPage/SavedJobItem.jsx
+++ src/components/SavedJobsPage/SavedJobItem.jsx
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 import { PanoramaSharp } from "@mui/icons-material";
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
+import { Typography, Grid } from '@mui/material';
 
 
 function SavedJobItem({ savedJob }) {
@@ -25,128 +25,81 @@ function SavedJobItem({ savedJob }) {
     }
 
     return (<>
-        <h1>This is an job item</h1>
-        <Box sx={{ borderRadius: '5px', boxShadow: 3, padding: '10px', border: 'solid grey 1px', display: "flex", justifyContent: 'space-between', marginBottom: 2 }}>
+
+        <Box >
+            <Box sx={{ borderRadius: '5px', boxShadow: 3, padding: '10px', border: 'solid grey 1px', display: "flex", justifyContent: 'space-between', marginBottom: 2 }}>
 
 
-            <Box sx={{ marginBottom: 1 }}>
-                {savedJob.title}
+                <Box>
+                    <Box sx={{ marginBottom: 1, marginTop: '15%'}}>
+                        <b>Job Title:</b> {savedJob.title}
+                    </Box>
+
+                    <Box sx={{ marginTop: '15%' }}>
+                        <b>Company Name:</b> {savedJob.company_name}
+                    </Box>
+                </Box>
+
+                <Box>
+
+
+                    <Box onClick={submitApplied}>
+                        {appliedJobsList.find((c) => c.id === savedJob.id) ? (
+                            <p>Applied</p>
+                        ) : (
+                            <Button
+                                variant='contained'
+                                sx={{ marginBottom: 1 }}
+                                onClick={() => {
+                                    dispatch({
+                                        type: "APPLY_JOB",
+                                        payload: savedJob,
+                                    });
+                                    history.push("/applied");
+                                }}
+                            >
+                                APPLY{" "}
+                            </Button>
+                        )}
+
+                    </Box>
+
+
+                    <Box>
+                        <Button
+                            variant='contained'
+                            sx={{ marginBottom: 1 }}
+                            onClick={() => {
+                                history.push(`/CandidateJobDetails/${savedJob.id}`);
+                                dispatch({
+                                    type: "VIEW_JOB_DETAILS",
+                                    payload: `${savedJob.id}`,
+                                });
+                            }}
+                        >
+                            DETAILS
+                        </Button>
+                    </Box>
+
+                    <Box>
+                        <Button
+                            variant='contained'
+                            sx={{ marginBottom: 1 }}
+                            onClick={() => {
+                                dispatch({
+                                    type: "DELETE_JOB",
+                                    payload: savedJob,
+                                });
+                            }}
+                        >
+                            REMOVE{" "}
+                        </Button>
+
+
+                    </Box>
+                </Box>
             </Box>
-
-            <Box sx={{ marginBottom: 1 }}>
-                {savedJob.company_name}
-            </Box>
-
-            <Box onClick={submitApplied}>
-                {appliedJobsList.find((c) => c.id === savedJob.id) ? (
-                    <p>Applied</p>
-                ) : (
-                    <Button
-                        onClick={() => {
-                            dispatch({
-                                type: "APPLY_JOB",
-                                payload: savedJob,
-                            });
-                            history.push("/applied");
-                        }}
-                    >
-                        APPLY{" "}
-                    </Button>
-                )}
-
-            </Box>
-
-
-            <Box>
-                <Button
-                    onClick={() => {
-                        history.push(`/CandidateJobDetails/${savedJob.id}`);
-                        dispatch({
-                            type: "VIEW_JOB_DETAILS",
-                            payload: `${params.id}`,
-                        });
-                    }}
-                >
-                    DETAILS
-                </Button>
-            </Box>
-
-            <Box>
-            <Button
-                onClick={() => {
-                    dispatch({
-                        type: "DELETE_JOB",
-                        payload: savedJob,
-                    });
-                }}
-            >
-                REMOVE{" "}
-            </Button>
-
-
-            </Box>
-
-
-
-
-
-
-
-
-
         </Box>
-        {/* <div key={job.id}>
-            <h4> {job.title} </h4>
-
-            <p> {job.company_name}</p>
-
-            <p> {job.company_address}</p>
-
-            <div onClick={submitApplied}>
-                {appliedJobsList.find((c) => c.id === job.id) ? (
-                    <p>Applied</p>
-                ) : (
-                    <button
-                        onClick={() => {
-                            dispatch({
-                                type: "APPLY_JOB",
-                                payload: job,
-                            });
-                            history.push("/applied");
-                        }}
-                    >
-                        APPLY{" "}
-                    </button>
-                )}
-            </div>
-
-            <button
-                onClick={() => {
-                    history.push(`/CandidateJobDetails/${job.id}`);
-                    dispatch({
-                        type: "VIEW_JOB_DETAILS",
-                        payload: `${params.id}`,
-                    });
-                }}
-            >
-                DETAILS
-            </button>
-
-            <button
-                onClick={() => {
-                    dispatch({
-                        type: "DELETE_JOB",
-                        payload: job,
-                    });
-                }}
-            >
-                REMOVE{" "}
-            </button>
-        </div> */}
-
-
-
-
 
     </>)
 }
--- src/components/SavedJobsPage/SavedJobsPage.jsx
+++ src/components/SavedJobsPage/SavedJobsPage.jsx
@@ -5,6 +5,8 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { PanoramaSharp } from "@mui/icons-material";
 import SavedJobItem from "./SavedJobItem";
+import { Box } from "@mui/system";
+import { Grid, List, Button } from "@mui/material";
 
 function SavedJobsPage() {
   const dispatch = useDispatch();
@@ -28,14 +30,23 @@ function SavedJobsPage() {
 
   return (
     <>
-      <h1>Saved Jobs</h1>
-      <section>
-        {savedJobsList.map((savedJob) => {
-          return (
-            <SavedJobItem key={savedJob.id} savedJob={savedJob}/>
-          );
-        })}
-      </section>
+      <Grid container spacing={2}>
+
+        <Grid item xs={2}>
+
+        </Grid>
+        <Grid item xs={8}>
+          <Grid item xs={8}>
+            <Button variant='contained' onClick={() => { window.history.back() }}>Back</Button>
+            <h1 >Saved Jobs</h1></Grid>
+          <List>
+            {savedJobsList.map((savedJob) =>
+              <SavedJobItem key={savedJob.id} savedJob={savedJob} />
+            )}
+          </List>
+        </Grid>
+        <Grid item xs={2}></Grid>
+      </Grid>
     </>
   );
 }
